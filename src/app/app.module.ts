a0a57e3b47c8ee735434c6d2fae48ce1ce0e064c
CORP\pbhat <pbhat@extremenetworks.com>
2023-10-18 13:05:34 +0530

    blog skeleton created,fix routing for tiles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BlogPostComponent
   ],
   imports: [
     BrowserModule,
