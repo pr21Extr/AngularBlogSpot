20641acad61ab08ae58bc17e19eaa641c41c662c
CORP\pbhat <pbhat@extremenetworks.com>
2023-10-19 15:34:56 +0530

    WIP add i18n

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ContentfullService } from '../services/contentfull.service';
 import { Observable } from 'rxjs';
+import '@angular/localize/init';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +10,21 @@ import { Observable } from 'rxjs';
 })
 export class HomeComponent implements OnInit {
 constructor(private ContentfullService : ContentfullService){
-  // this.ContentfullService.getAllEntries();
 }
+
 blogPosts$ : Observable <any> | undefined;
+catTitle = $localize`:@@cat-title|Default Cat Title`;
+  catAuthor = $localize`:@@cat-author|Default Cat Author`;
+  catSummary = $localize`:@@cat-summary|Default Cat Summary`;
+  carTitle = $localize`:@@car-title|Default Car Title`;
+  carAuthor = $localize`:@@car-author|Default Car Author`;
+  carSummary = $localize`:@@car-summary|Default Car Summary`;
+  foodTitle = $localize`:@@food-title|Default Food Title`;
+  foodAuthor = $localize`:@@food-author|Default Food Author`;
+  foodSummary = $localize`:@@food-summary|Default Food Summary`;
+
 
 ngOnInit(): void {
   this.blogPosts$ = this.ContentfullService.getAllEntries();
-  
 }
 }
